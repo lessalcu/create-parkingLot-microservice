ea040bcfd3cde318441a58f915b17da88f4cf43d
Meybili <109104859+meybili19@users.noreply.github.com>
2025-02-12 18:52:28 -0500

    feat:

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ const parkingLotRoutes = require('./routes/parkingLotRoutes');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+    origin: "*", // Permite cualquier origen
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(bodyParser.json());
 app.use(parkingLotRoutes);
 
