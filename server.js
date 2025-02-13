72544b6e1bfc6530cdaddfd35454af1042b881be
Meybili <109104859+meybili19@users.noreply.github.com>
2025-02-12 20:02:32 -0500

    feat:

--- server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./src/app');
-const PORT = process.env.PORT || 6000;
-
-app.listen(PORT, "0.0.0.0", () => { // Escuchar en todas las IPs
-    console.log(`🚀 Server running at http://0.0.0.0:${PORT}`);
-});
--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-const cors = require('cors');
 const express = require('express');
-const bodyParser = require('body-parser');
-const parkingLotRoutes = require('./routes/parkingLotRoutes');
-require('dotenv').config();
+const cors = require('cors');
+const parkingRoutes = require('./routes/parkingLotRoutes');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
-// Permitir CORS para todas las solicitudes
-app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
+// Habilitar CORS en todas las rutas de este microservicio
+app.use(cors());
 
-app.use(express.json()); 
-app.use(bodyParser.json());
-app.use(parkingLotRoutes);
+app.use(express.json());
 
-module.exports = app;
+app.use(parkingRoutes);
+
+const PORT = process.env.PORT || 6000;
+app.listen(PORT, () => {
+  console.log(`Parking Lot Registration microservice running on port ${PORT}`);
+});
