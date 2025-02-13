39c15fa8181e7c25fc29c1f4d25ef86af064b841
Meybili <109104859+meybili19@users.noreply.github.com>
2025-02-12 20:30:07 -0500

    feat:

--- src/app.js
+++ src/app.js
@@ -7,11 +7,14 @@ dotenv.config();
 
 const app = express();
 
-// Habilitar CORS en todas las rutas de este microservicio
-app.use(cors());
+// 🔹 Permitir cualquier origen
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json());
-
 app.use(parkingRoutes);
 
 const PORT = process.env.PORT || 6000;
