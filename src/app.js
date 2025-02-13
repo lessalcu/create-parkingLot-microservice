2c49e7b3af0a7a8dd0326fae8e691503efd27512
Meybili <109104859+meybili19@users.noreply.github.com>
2025-02-12 19:54:35 -0500

    feat:

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
-const express = require('express');
 const cors = require('cors');
+const express = require('express');
 const bodyParser = require('body-parser');
 const parkingLotRoutes = require('./routes/parkingLotRoutes');
 require('dotenv').config();
 
 const app = express();
 
+// Permitir CORS para todas las solicitudes
 app.use(cors({
-    origin: "*", // Permite cualquier origen
-    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
+app.use(express.json()); 
 app.use(bodyParser.json());
 app.use(parkingLotRoutes);
 
