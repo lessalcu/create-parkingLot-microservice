38011d651d5eb17c80e21bad3daec708bc824edb
Meybili <109104859+meybili19@users.noreply.github.com>
2025-02-12 19:01:56 -0500

    feat:

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 const PORT = process.env.PORT || 6000;
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => { // Escuchar en todas las IPs
+    console.log(`🚀 Server running at http://0.0.0.0:${PORT}`);
 });
